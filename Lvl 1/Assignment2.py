8536dded4aef87310eccc5cf1cc1ee1782220c9b
msramos123 <39749911+msramos123@users.noreply.github.com>
2018-05-30 09:12:47 +0800

    Add files via upload

---
+++ Lvl 1/Assignment2.py
@@ -0,0 +1,14 @@
+number = random.randint(0, 9)
+#print (number)
+
+while True:
+    print('Guess the number from 0 to 9')
+    guess = input()
+    guess = int(guess)
+    if guess == number:
+        print("Correct!")
+        break
+    else:
+        print("Wrong, try again!")
+
+
